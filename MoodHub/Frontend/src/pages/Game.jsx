d9b5f85ba437d4ed772266e2a2e5c730730f8a57
namrata <210711144+np212000@users.noreply.github.com>
2026-09-07 18:08:55 +0530

    Add frontend CI pipeline

--- MoodHub/Frontend/src/pages/Game.jsx
+++ MoodHub/Frontend/src/pages/Game.jsx
@@ -1,52 +1,76 @@
 import React from "react";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import API from "../services/api";
 import VoteCard from "../components/VoteCard";
 
 function Game()
 {
-  const [question,setQuestion] = useState();
+  const [question, setQuestion] = useState();
 
-  //fetching All the Questions
-  const fetchQuestions = async()=>
+  // Function used to refresh questions after voting
+  const fetchQuestions = async () =>
   {
-    try 
+    try
     {
-     const res = await API.get("/questions/never-have-i-ever");
-     console.log("API Response:", res.data);
-     setQuestion(res.data); 
-    } 
-    catch (error) 
+      const res = await API.get("/questions/never-have-i-ever");
+
+      console.log("API Response:", res.data);
+
+      setQuestion(res.data);
+    }
+    catch (error)
     {
       console.log(error);
     }
   };
-  //Fetched Questions loades when screen loads
-  useEffect(()=>
+
+  // Fetch question when the screen loads
+  useEffect(() =>
   {
-    fetchQuestions();
-  },[]);
+    const loadQuestion = async () =>
+    {
+      try
+      {
+        const res = await API.get("/questions/never-have-i-ever");
+
+        console.log("API Response:", res.data);
+
+        setQuestion(res.data);
+      }
+      catch (error)
+      {
+        console.log(error);
+      }
+    };
+
+    loadQuestion();
+  }, []);
+
   return(
     <>
-    <div className="min-h-screen bg-gray-100 p-5 flex justify-center">
-      <div className="w-full max-w-xl">
-        <h2 className="text-3xl font-bold text-center mb-6">
-          Never Have I Ever 🤔
-        </h2>
+      <div className="min-h-screen bg-gray-100 p-5 flex justify-center">
 
-        <div className="space-y-4">
-       {question && (
-        <VoteCard
-          key={question._id}
-          question={question}
-        refresh={fetchQuestions}
-        />
-  )}
-</div>
+        <div className="w-full max-w-xl">
+
+          <h2 className="text-3xl font-bold text-center mb-6">
+            Never Have I Ever 🤔
+          </h2>
+
+          <div className="space-y-4">
+
+            {question && (
+              <VoteCard
+                key={question._id}
+                question={question}
+                refresh={fetchQuestions}
+              />
+            )}
+
+          </div>
+
+        </div>
 
       </div>
-
-    </div>
     </>
   );
 }
